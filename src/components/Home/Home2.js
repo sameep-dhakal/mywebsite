06cb999598af6964a06d7b1368441d4c82a2c694
Sameep Dhakal <sameep.dhakal721108@gmail.com>
2023-11-14 18:24:21 +0545

    companies added

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -9,55 +9,47 @@ import {
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn, FaFacebook } from "react-icons/fa";
+import { ImHipster2 } from "react-icons/im";
 
 function Home2() {
   return (
-    <Container fluid className="home-about-section" id="about">
+    <Container fluid className="home-about-section items-center justify-center" id="about">
       <Container>
         <Row>
-          <Col md={8} className="home-about-description">
+          <Col md={12} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              My <span className="purple"> Experiences </span>
             </h1>
-            <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> Python, C, C++ and Javascript </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Machine learning products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                Web Technologies.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
-            </p>
+            <h2 className="home-about-body text-center">
+    I am a Software Developer at 
+    <span className="company-name" data-hover-content="Year: 2021-2023"> 
+        <a href="https://www.processfeedback.org" target="_blank" rel="noreferrer">
+            Process Feedback
+        </a>
+    </span>
+    <br />
+    Instructor at 
+    <span className="company-name" data-hover-content="Year: 2019-2021">
+        Advanced College of Engineering and Management
+    </span>
+    <br />
+    Software Engineer at 
+    <span className="company-name" data-hover-content="Year: 2017-2019">
+        BP Eye Foundation
+    </span>
+    <br />
+    Data Science Fellow at 
+    <span className="company-name" data-hover-content="Year: 2015-2017">
+        Deerhold
+    </span>
+</h2>
+
           </Col>
-          <Col md={4} className="myAvtar">
+          {/* <Col md={4} className="myAvtar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
-          </Col>
+          </Col> */}
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
