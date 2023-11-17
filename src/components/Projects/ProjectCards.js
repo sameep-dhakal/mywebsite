c2682b38dc5083cf8a1bd2b193430ab255522f3e
Sameep Dhakal <sameep.dhakal721108@gmail.com>
2023-11-17 18:53:48 +0545

    projects added

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -4,12 +4,14 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
+import "./project.css"
+
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" className="card-image" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title className="card-title-custom">{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import pf from "../../Assets/Projects/processreport.png";
 import summarizer from "../../Assets/Projects/summarizer.png";
+import eyewear from "../../Assets/Projects/virtualtryon.png";
+import ecommerce from "../../Assets/Projects/ecommerce.png";
+import messianalysis from "../../Assets/Projects/messianalysis.png";
+
 import MainLayout from "../layout/mainLayout/mainLayout"
 
 function Projects() {
@@ -34,6 +38,38 @@ function Projects() {
               isBlog={false}
               title="Text Summarizer"
               description="Text Summarizer is an intelligent tool that condenses lengthy paragraphs into a concise summary, capturing the most critical information and main ideas."
+              demoLink=" "
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+             imgPath={eyewear}
+              isBlog={false}
+              title="Virtual Eyewear Tryon"
+              description="Virtual eyewear tryon captures your image and places the elected the eye products on your face so that you know how the eywear looks on your face. Basically like a Snapchat filter."
+              demoLink=" "
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+             imgPath={messianalysis}
+              isBlog={false}
+              title="Football Data Analytics"
+              description="Football Data analysis is the data analytics project thet helps to visualize different kinds 
+              of the statistics of the footballer. For now I have analyzed data for one player Lionel Messi."
+              demoLink=" "
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+             imgPath={ecommerce}
+              isBlog={false}
+              title="Ecommerce Products classifiction"
+              description="Ecommerce Products classification reads the image and classifies the images according to the classes they are predefined
+              This is the basic implementation of the multiple classification using CNN and beginner friendly."
               demoLink=" "
             />
           </Col>
