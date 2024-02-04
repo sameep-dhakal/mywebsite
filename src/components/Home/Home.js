859b446c7ceb4ffa625e4fbc7bede12eee81ada9
Sameep Dhakal <sameep.dhakal721108@gmail.com>
2024-02-04 09:14:09 +0545

    pic size updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,47 +4,56 @@ import homeLogo from "../../Assets/home-main.jpg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import MainLayout from "../layout/mainLayout/mainLayout"
+import MainLayout from "../layout/mainLayout/mainLayout";
 
 function Home() {
   return (
     <MainLayout>
-    <section>
-      <Container fluid className="home-section" id="home">
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hello and Namaste!{" "}
-                <span className="greeting" role="img" aria-labelledby="namaste">
-                  🙏
+      <section>
+        <Container fluid className="home-section" id="home">
+          <Particle />
+          <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">
+                  Hello and Namaste!{" "}
+                  <span
+                    className="greeting"
+                    role="img"
+                    aria-labelledby="namaste"
+                  >
+                    🙏
                   </span>
-              </h1>
+                </h1>
 
-              <h1 className="heading-name">
-                It's Me
-                <strong className="main-name"> Sameep Dhakal</strong>
-              </h1>
+                <h1 className="heading-name">
+                  It's Me
+                  <strong className="main-name"> Sameep Dhakal</strong>
+                </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+                <div style={{ padding: 50, textAlign: "left" }}>
+                  <Type />
+                </div>
+              </Col>
 
-            <Col md={5} style={{ paddingBottom: 20, zIndex: 1 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid rounded-circle"
-                style={{ height: "400px", width: "400px", objectFit: "cover" ,border:'2px solid'}}
-              />
-            </Col>
-          </Row>
+              <Col md={5} style={{ paddingBottom: 20, zIndex: 1 }}>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid rounded-circle"
+                  style={{
+                    height: "450px",
+                    width: "400px",
+                    objectFit: "",
+                    border: "2px solid",
+                  }}
+                />
+              </Col>
+            </Row>
+          </Container>
         </Container>
-      </Container>
-      <Home2 />
-    </section>
+        <Home2 />
+      </section>
     </MainLayout>
   );
 }
